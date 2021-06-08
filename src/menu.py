75a3134a05d35707cda55292983c117f7d34be8d
Okan Torun <torun.okn@gmail.com>
2021-06-08 13:39:35 +0300

    Menu updated

--- src/menu.py
+++ src/menu.py
@@ -9,27 +9,27 @@ import random
 
 
 print("""
----- Çoban-Z Sürü Drone Algoritmaları Kumanda Modülü ----
+---- Çoban-Z Swarm Drone Algorithms Control Module ----
 ---------------------------------------------------------
-Dikkat !!  Her formasyon için minimum bir drone sayısı vardır 
+Attention !! There is a minimum number of drones for each formation
 
-Üçgen --> 3 
-Kare --> 4 
+Triangle --> 3 
+Square --> 4 
 V --> 5
-Beşgen --> 5
-Hilal --> 6
-Yıldız --> 10
+Pentagon --> 5
+Crescent --> 6
+Star --> 10
 
 """)
     
-drone_number = int(input("Lütfen kaç drone ile başlamak istediğinizi giriniz : "))
+drone_number = int(input("Please enter how many drones you want to start with : "))
 print("------------------------------------------------")
 print("------------------------------------------------")
-take_off_type = int(input("""Dronların eşzamanlı kaldırılmasını istiyorsanız --> 1 
+take_off_type = int(input("""If you want simultaneous take off of drones --> 1 
 Teker teker kalkmalarını istiyorsanız --> 2 : 
 """))
-hight = float(input("kaç metre yüksekliğe çıkılsın :  "))
-distance = float(input("elemanlar arasındaki mesafeyi giriniz :  "))
+hight = float(input("how many meters high?:  "))
+distance = float(input("enter the distance between elements :  "))
 
 swarm = Crazyswarm()
 timeHelper = swarm.timeHelper
@@ -43,42 +43,42 @@ if take_off_type == 1 :
 elif take_off_type == 2:
     swarm2.takeoff_scyn(hight, 5)
 else :
-    print("hatalı giris yaptiniz")
+    print("you entered wrong")
 
 
 
 if drone_number == 3:
-    print("seçebileceğiniz formasyonlar : ")
-    print("1 --> Üçgen ")
+    print("formations to choose from : ")
+    print("1 --> Triangle ")
 elif drone_number ==4 :
-    print("seçebileceğiniz formasyonlar : ")
-    print("1 --> Üçgen ")
-    print("2 --> Kare ")
+    print("formations to choose from : ")
+    print("1 --> Triangle ")
+    print("2 --> Square ")
 elif drone_number ==5 :
-    print("seçebileceğiniz formasyonlar : ")
-    print("1 --> Üçgen ")
-    print("2 --> Kare ")
-    print("3 --> V formasyonu ")
-    print("4 --> Beşgen formasyonu ")
+    print("formations to choose from : ")
+    print("1 --> Triangle ")
+    print("2 --> Square ")
+    print("3 --> V formation ")
+    print("4 --> Pentagon formation ")
 elif drone_number == 6 :
-    print("seçebileceğiniz formasyonlar : ")
-    print("1 --> Üçgen ")
-    print("2 --> Kare ")
-    print("3 --> V formasyonu ")
-    print("4 --> Beşgen formasyonu ")
-    print("5 --> Hilal formasyonu ")
+    print("formations to choose from : ")
+    print("1 --> Triangle ")
+    print("2 --> Square ")
+    print("3 --> V formation ")
+    print("4 --> Pentagon formation ")
+    print("5 --> Crescent formation ")
     
 elif drone_number > 6 :
-    print("seçebileceğiniz formasyonlar : ")
-    print("1 --> Üçgen ")
-    print("2 --> Kare ")
-    print("3 --> V formasyonu ")
-    print("4 --> Beşgen formasyonu ")
-    print("5 --> Hilal formasyonu ")
-    print("6 --> Yıldız fprmasyonu")
+    print("formations to choose from : ")
+    print("1 --> Triangle ")
+    print("2 --> Square ")
+    print("3 --> V formation ")
+    print("4 --> Pentagon formation ")
+    print("5 --> Crescent formation ")
+    print("6 --> Star formation")
 
     
-formation_id = int(input("formasyon numarasını giriniz : "))
+formation_id = int(input("enter formation number : "))
 
 
 if formation_id == 1:
@@ -100,23 +100,23 @@ cur_x = 0
 cur_y = 0
 
 while True :
-    print("""Formasyonu güncellemek için --> 1
-Yükselme veya Alçalma için --> 2  
-Rotasyon yaptırmak için  --> 3
-Navigasyon yaptırmak için --> 4
-Sürü ayrılması için  --> 5
-Dönerek Navigasyon için --> 6
-Sıralı iniş için --> 7
+    print("""To update the formation --> 1
+For Ascension or Descent --> 2  
+For  rotation  --> 3
+For navigation --> 4
+For swarm separation  --> 5
+For Turning Navigation --> 6
+For sequential landing --> 7
 """)
-    selection = int(input("seçiminiz : "))
+    selection = int(input("Selection : "))
     if selection == 1 :
-        print(""" eleman eklemek için --> 1
-        eleman çıkarmak için --> 2
-        elamanlar arası mesafeyi güncellemek için --> 3
-        formasyon değiştirmek için --> 4
-        kare formasyonundayken eleman ekleme için --> 5
+        print(""" to add an element --> 1
+        to remove element --> 2
+        to update the distance between elements --> 3
+        to change formation --> 4
+        for adding elements while in square formation --> 5
         """)
-        selection2 = int(input("seçiminiz : "))
+        selection2 = int(input("Selection : "))
 
         if selection2 == 1 :
             swarm2.add_crazyflie(allcfs.crazyflies[drone_number])
@@ -125,52 +125,52 @@ Sıralı iniş için --> 7
             swarm2.take_formation(distance,formation_id,hight,cur_x,cur_y)
         elif selection2 == 2 :
             rastgele = random.randint(0,drone_number-1)
-            print("rastgele eleman çıkarılıyor ...")
+            print("randomly removing element ...")
             swarm2.remove_crazyflie(swarm2.crazyflies[rastgele])
             drone_number = drone_number -1 
             swarm2.take_formation(distance,formation_id,hight,cur_x,cur_y)
         elif selection2 == 3 :
-            aralık = int(input("elemanlar arasındaki istediğiniz mesafeyi giriniz : "))
+            aralık = int(input("Enter the distance you want between the elements : "))
             distance = aralık
             swarm2.take_formation(aralık,formation_id,hight,cur_x,cur_y)
         elif selection2 == 4 :
-            print("seçebileceğiniz formasyonlar : ")
-            print("1 --> Üçgen ")
-            print("2 --> Kare ")
-            print("3 --> V formasyonu ")
-            print("4 --> Beşgen formasyonu ")
-            print("5 --> Hilal formasyonu ")
-            print("6 --> Yıldız formasyonu")
-            print("7 --> Daire formasyonu")
+            print("formations to choose from : ")
+            print("1 --> Triangle ")
+            print("2 --> Square ")
+            print("3 --> V formation ")
+            print("4 --> Pentagon formation ")
+            print("5 --> Crescent formation ")
+            print("6 --> Star formation")
+            print("7 --> Circle formation")
 
-            selection3 = int(input("seçiminiz : "))
+            selection3 = int(input("Selection : "))
             formation_id = selection3
             swarm2.change_formation(selection3,distance,cur_x,cur_y)
             
     elif selection  == 2 :
-        vertical_move = float(input("Sürüyü hangi yüksekliğe hareket ettirmek istersiniz : "))
+        vertical_move = float(input("To what height do you want to move the Swarm : "))
         swarm2.takeoff_ascyn(vertical_move, 5)
         hight = vertical_move    
 
     elif selection  == 3 :
-        rot = int(input("kac derece rotasyon yaptırmak istiyorsunuz : "))
+        rot = int(input("How many degrees do you want to rotate? : "))
         print(cur_x)
         print(cur_y)
         swarm2.rotasyon_3(rot,cur_x, cur_y, hight,distance)
         
 
     elif selection  == 4 :
-        x = float(input("navigasyon yapmak istediğiniz noktanın x kordinatını giriniz : " ))
-        y = float(input("navigasyon yapmak istediğiniz noktanın y kordinatını giriniz : " ))
+        x = float(input("Enter the x coordinate of the point you want to navigate : " ))
+        y = float(input("Enter the y-coordinate of the point you want to navigate : " ))
         swarm2.navigation_2([x,y,hight],10)
         cur_x = x 
         cur_y = y
         
     elif selection  == 5 :
-        print("sürü ayrılması yapmak için minimum 6 iha bulunmalıdır ")
+        print("There must be a minimum of 6 UAVs to make a swarm separation ")
         if drone_number > 5 :
-            print("test için  bir üçgen ve istediğimiz başka bir formasyon :  ")
-            sep_swarm = int(input("lütfen istediğiniz diğer formasyonu giriniz "))
+            print("A triangle for testing and another pattern we want :  ")
+            sep_swarm = int(input("Please enter the other formation you want "))
             crazyflies_1 = allcfs.crazyflies[0:3]
             crazyflies_2= allcfs.crazyflies[3:drone_number]
             swarm_3,swarm_4 = swarm2.swarm_separate(crazyflies_1, crazyflies_2)
@@ -181,7 +181,7 @@ Sıralı iniş için --> 7
             swarm_3.navigation_2([-4,-4,hight+1], 10)
             swarm_4.navigation_2([4,4,hight-1], 10)
 
-            print("tekrar birleşim yapılıyor....")
+            print("Reunification is taking place....")
             timeHelper.sleep(10)
             swarm2.take_formation(distance,sep_swarm, hight, 0, 0)
     #not working like expected
